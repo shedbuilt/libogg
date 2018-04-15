7014486f75fb1b14cc9e259d055cd48543617565
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:54:26 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 ./configure --prefix=/usr \
             --disable-static \
             --docdir=/usr/share/doc/libogg-1.3.3 && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
